34a0f01febb67b9f3aee7d5e3f8740c6e7c4510f
rum-yasuhiro <banana.himuken@gmail.com>
2020-11-11 19:07:35 +0900

    update /util/statistics.py

--- utils/statistics.py
+++ utils/statistics.py
@@ -1,23 +1,14 @@
 import statistics
 import numpy as np
 
-def sem(quad_list):
-    length = len(quad_list[0])
-    sem_list = []
-    for i in range(length):
-        xth = [list_seed[i] for list_seed in quad_list]
-        std = statistics.stdev(xth)
-        sem_list.append(std / np.sqrt(len(xth)))
-    return sem_list
+def sem(_list):
+    std = statistics.stdev(_list)
+    standard_error_mean = std / np.sqrt(len(_list))
+    return standard_error_mean
 
-def mean(quad_list): 
-    length = len(quad_list[0])
-    mean_list = []
-    for i in range(length):
-        xth = [list_seed[i] for list_seed in quad_list]
-        mean = statistics.mean(xth)
-        mean_list.append(mean)
-    return mean_list
+def mean(_list): 
+    mean_value = statistics.mean(_list)
+    return mean_value
 
 
 
